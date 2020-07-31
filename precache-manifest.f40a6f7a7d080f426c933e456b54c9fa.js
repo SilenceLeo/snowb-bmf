be3a939f2e538e5216326ea4903acfad6b9bd29a
Leo <ly.liyang521@gmail.com>
2020-07-31 14:29:49 +0800

    Updates

---
+++ precache-manifest.f40a6f7a7d080f426c933e456b54c9fa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b358164f5e8508a40b3d422f371c5be",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1090cd700a1ef23ddff0",
+    "url": "./static/css/main.2cc839bc.chunk.css"
+  },
+  {
+    "revision": "f4ccfc2189e469562de9",
+    "url": "./static/js/2.fc7be827.chunk.js"
+  },
+  {
+    "revision": "49f7d13633d57640d63a355218ff3659",
+    "url": "./static/js/2.fc7be827.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd15a1a8e12558fc6f9b8480086fdf72",
+    "url": "./static/js/RectanglePacker.654b1e16b59dc2cc713a.worker.js"
+  },
+  {
+    "revision": "1090cd700a1ef23ddff0",
+    "url": "./static/js/main.2cc98221.chunk.js"
+  },
+  {
+    "revision": "6f7eaa23c455e61dde68",
+    "url": "./static/js/runtime-main.8eb0ca99.js"
+  }
+]);
