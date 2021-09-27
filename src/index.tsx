a6edec53188d28ddca04fac8420477c8bf016209
Leo <ly.liyang521@gmail.com>
2021-09-28 01:30:28 +0800

    feat: add Sentry

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import * as Sentry from '@sentry/react'
+import { Integrations } from '@sentry/tracing'
 
 import App from './app/App'
 
 import * as serviceWorkerRegistration from './serviceWorkerRegistration'
+
+if (process.env.NODE_ENV === 'production') {
+  Sentry.init({
+    dsn: 'https://007c463bad354a5baf9a11d8e9d7c8a6@o501223.ingest.sentry.io/5981296',
+    integrations: [new Integrations.BrowserTracing()],
+    tracesSampleRate: 1.0,
+  })
+}
 
 ReactDOM.render(<App />, document.getElementById('root'))
 
